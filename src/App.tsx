7d1b0abda1bcd2ea096fa87e762e974f55fe97ca
Alberto Martinez de Murga Ramirez <alberto@threkk.com>
2022-10-24 16:08:21 +0900

    chore: storage commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,35 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import './App.scss'
 
 class App extends Component {
+  state = {
+
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.tsx</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+      <main className="container App bg-dark">
+        <div className="columns col-gapless">
+          <div className="col-xl-6 col-lg-8 col-md-10 col-sm-12 col-xs-12  col-mx-auto">
+            <h1>When it is</h1>
+            <div className="input-group" >
+              <input className="form-input input-sm" type="text" />
+              <select className="form-select select-sm">
+                <option>Example</option>
+              </select>
+            </div>
+            <h1>is</h1>
+            <div className="input-group">
+              <input className="form-input input-sm" type="text" />
+              <select className="form-select select-sm">
+                <option>Example</option>
+              </select>
+            </div>
+          </div>
+        </div>
+      </main>
+    )
   }
 }
 
-export default App;
+export default App
